47c66bb9d615c6a046d157488145e3ecb3fba10c
SEH00N <06psh0501@gmail.com>
2023-05-15 23:02:09 +0900

    겜프

---
+++ 2023겜프/23.05.15/cpp/BombMan/GameLogic.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <vector>
+
+//#define HORIZONTAL	20
+//#define VERTICAL	20
+
+const int HORIZONTAL = 21;
+const int VERTICAL = 20;
+
+typedef struct POS_t {
+	int x;
+	int y;
+}POS, *PPOS;
+
+typedef struct PLAYER_t {
+	POS tPos;			// À§Ä¡
+	POS tNewPos;
+	int iBombPower;		// ÆøÅº ÆÄ¿ö
+	int iBombCount;		// ÆøÅº °³¼ö
+	bool bSlime;		// ½½¶óÀÓ
+	bool bWallPush;		// º®Çª½Ã
+	bool bPushToggle;	// º® ¹Ð±â ½ºÀ§Ä¡
+}PLAYER, *PPLAYER;
+
+typedef struct BOOM_t {
+	int x;
+	int y;
+	int life;
+	bool bDie;
+}BOOM, *PBOOM;
+
+enum class MAPTYPE {
+	WALL = '0',
+	ROAD = '1',
+	START = '2',
+	END = '3',
+	WATERBOMB = 'b',
+	TWINKLE = 'p',
+	POWER = '4',
+	SLIME = '5',
+	PUSH = '6'
+};
+
+void AsciiArt();
+void Init(char _cMaze[VERTICAL][HORIZONTAL], PPLAYER _pPlayer, PPOS _pStartPos, PPOS _pEndPos);
+void Update(char _cMaze[VERTICAL][HORIZONTAL], PPLAYER _pPlayer, std::vector<BOOM>& _vecBomb, std::vector<POS>& _vecBoomEffect);
+void Render(char _cMaze[VERTICAL][HORIZONTAL], PPLAYER _pPlayer, std::vector<POS>& _vecBoomEffect);
+void Event(std::vector<BOOM>& _vecBomb, std::vector<POS>& _vecBoomEffect);
+
+bool GetItem(char _cItem, PPLAYER _pPlayer);
+
+void BombCreate(char _cMaze[VERTICAL][HORIZONTAL], PPLAYER _pPlayer, std::vector<BOOM>& _vecBomb);
+void Fire(char _cMaze[VERTICAL][HORIZONTAL], PPLAYER _pPlayer, POS _boomPos, std::vector<POS>& _vecBoomEffect);
---
+++ 2023겜프/23.05.15/cpp/BombMan/test.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <mmsystem.h>
+#include "console.h";
+#include "GameLogic.h";
+#include "StartScene.h";
+
+#pragma comment(lib, "winmm.lib")
+
+using namespace std;
+
+int main()
+{
+	char cMaze[VERTICAL][HORIZONTAL] = {};
+	PLAYER tPlayer = {};
+	POS tStartPos = {};
+	POS tEndPos = {};
+	vector<BOOM> vecBoom;
+	vector<POS> vecBoomEffect;
+
+	Init(cMaze, &tPlayer, &tStartPos, &tEndPos);
+
+	while (true) {
+		system("cls");
+		AsciiArt();
+		int iMenu = GameMenu();
+
+		if (iMenu == 0)
+			break;
+		else if (iMenu == 1)
+			GameInfo();
+		else if (iMenu == 2)
+		{
+			Sleep(100);
+			system("cls");
+			cout << "게임을 종료합니다." << endl;
+
+			for (int i = 0; i < 3; ++i)
+			{
+				cout << '\r' << 3 - i << "...";
+				Sleep(1000);
+			}
+
+			return 0;
+		}
+	}
+
+	system("cls");
+	clock_t oldTime, currentTime;
+	oldTime = clock();
+
+	while (true)
+	{
+		Go2xy(0, 0);
+		Update(cMaze, &tPlayer, vecBoom, vecBoomEffect);
+		Render(cMaze, &tPlayer, vecBoomEffect);
+		Event(vecBoom, vecBoomEffect);
+
+		if (tPlayer.tPos.x == tEndPos.x && tPlayer.tPos.y == tEndPos.y)
+		{
+			PlaySound(TEXT("random.wav"), 0, SND_FILENAME | SND_ASYNC);
+			Sleep(1000);
+			break;
+		}
+
+		while (true)
+		{
+			currentTime = clock();
+			if(currentTime - oldTime >= 1000/30.f * 2.f)
+			{
+				oldTime = currentTime;
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
